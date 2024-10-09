d090809599dd0d402810f832a70b4a095d12ec9e
Styri <styltia@hotmail.com>
2024-10-09 20:39:33 +0300

    Update properties of model to better match db table

--- back-end/Models/Recipe.cs
+++ back-end/Models/Recipe.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace RecipeManager.Models
@@ -5,16 +6,20 @@ namespace RecipeManager.Models
     public class Recipe
     {
         [JsonPropertyName("recipeId")]
-        public int RecipeId { get; set; }
+        public int RecipeId { get; private set; }
 
         [JsonPropertyName("name")]
+        [Required]
+        [StringLength(250)]
         public required string Name { get; set; }
 
         [JsonPropertyName("description")]
+        [Required]
+        [StringLength(500)]
         public required string Description { get; set; }
 
         [JsonPropertyName("category")]
-        public required string Category { get; set; }
+        public required string Category { get; set; } ="Other";
 
         [JsonPropertyName("favorite")]
         public bool Favorite { get; set; }
